db6ad480fdc0a7d120fb7fbbdb10200bbdabfedb
ShehabTaha <shehabtaha783@gmail.com>
2025-11-05 19:42:17 +0200

    V16 last

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import events, { EventItem } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const page = async () => {
-  "use cache";
-  cacheLife("seconds");
   // const response = await fetch(`${BASE_URL}/api/events`)
   // const {events} = await response.json()
   return (
