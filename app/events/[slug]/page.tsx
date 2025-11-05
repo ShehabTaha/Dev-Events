e4b77e6105666390b908fdd83ca14caed87472e7
ShehabTaha <shehabtaha783@gmail.com>
2025-11-05 09:55:56 +0200

    V16 final

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import EventDetails from "@/components/EventDetails";
 import { Suspense } from "react";
+import EventDetails from "@/components/EventDetails";
 
 const EventDetailsPage = async ({
   params,
@@ -7,14 +7,13 @@ const EventDetailsPage = async ({
   params: Promise<{ slug: string }>;
 }) => {
   const slug = params.then((p) => p.slug);
+
   return (
     <main>
       <Suspense fallback={<div>Loading...</div>}>
-        {" "}
         <EventDetails params={slug} />
       </Suspense>
     </main>
   );
 };
-
 export default EventDetailsPage;
